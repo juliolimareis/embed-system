4f6c78434e40e8b019ad6cc3e8f24a98419ac0c7
Júlio César Lima Reis <julio.reis@drtis.com.br>
2025-04-15 19:07:48 -0300

    Fix: Add allow no iframe para testes

--- app/(main)/module/page.tsx
+++ app/(main)/module/page.tsx
@@ -3,10 +3,11 @@
 export default function ModulePage() {
   return (
     <div className="w-full h-screen">
-      <iframe 
-        src={"https://develop.telemedicina.drtis.com.br"}
+      <iframe
         width="100%"
         height="100%"
+        src={"https://develop.telemedicina.drtis.com.br"}
+        allow="camera; microphone; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       />
     </div>
   )
